9e7ccea04bf9412f0cc6ccdad636cceff309638e
dzamilabaca-droid <dzamila.baca@ucas-edu.net>
2026-08-20 08:52:10 -0600

    hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+# comments
+
+print("Good Morning") # This is an output
+print("This is programming 1")
+print("As usual, we are experiencing technical difficulties")
+
+# input 
+name = input ("what is your name: ")
+
+print ("Welcome to class " + name)
+
+print("What did you do this weekend: ")
+weekend = input()
+print("That is super cool I wish I " + weekend + " this weekend.")
+
---
+++ variables.py
@@ -0,0 +1,20 @@
+# Dzamila Baca, Variables Notes
+
+# Create a variable
+name = "Gabriel"
+
+print("Hello " + name)
+
+# reset variable
+name = "Gadiel"
+
+print("Sorry I meant " + name)
+
+
+# another variable example
+age = 16
+
+print("Wow you are", age)
+print("Oh wait your birthday was yesterday")
+age += 1
+print("So you are", age)

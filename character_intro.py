d4372c2197516393913fe9e6987ea48089db882a
dzamilabaca-droid <dzamila.baca@ucas-edu.net>
2026-08-21 09:20:56 -0600

    character_intro.py

--- character_intro.py
+++ character_intro.py
@@ -1,7 +1,12 @@
 # Dzamila Baca, Character Introduction
 
+# create a variable
 name = "Sam"
+
 age = 24
+
 job = "pediatrician"
+
 country = "Chile"
+
 print(f"Hi, my name is {name}. I am {age} years old. I work as a {job}, and I'm from {country}")
